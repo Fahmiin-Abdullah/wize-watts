3f1c267552610c295979b29360a0c2ac5b289b22
fahmiin <fahmiin@tyne-solutions.com>
2020-07-14 00:36:11 +0800

    Added HTTPS prepend when in production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,6 +21,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        if ($this->app->environment() === 'production'){
+          $this->app['request']->server->set('HTTPS', true);
+        }
+
         Schema::defaultStringLength(191);
 
         View::composer(['*'], function($view) {
